1e1ac7f77e487c2539e3fa1afdeb0798dfcb431f
geetha-5656 <geethadoreswamy1234@gmail.com>
2025-12-10 01:10:48 +0530

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,24 @@
 // to access the variable cart outside of the cart.js file
 export const cart =[];
+
+// create a function to handle add to cart button 
+  export function addToCart(productId){
+     let matchingItem;
+      //loop through cart array to check if the product is already in the cart.
+      cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      }
+      else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+  }
+
